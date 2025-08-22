519ecdb783a52c51f3b7674c7239bb06b8335638
Pavan G R <115134463+Pavan-gr07@users.noreply.github.com>
2025-08-22 23:57:39 +0530

    some fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import Navbar from "../layout/Navbar";
+import Banner from "../section/Home/Banner";
+import UpcomingTrip from "../section/Home/UpcomingTrip";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Banner />
+      <UpcomingTrip />
     </div>
   );
 };
---
+++ src/section/Home/UpcomingTrip.jsx
@@ -0,0 +1,9 @@
+const UpcomingTrip = () => {
+  return (
+    <div>
+      <h1>Upcoming Trips</h1>
+    </div>
+  );
+};
+
+export default UpcomingTrip;
